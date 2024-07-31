dac8b7a6437e781e16b20b8a1377a7e0a6a622fe
Joao Pedro Schmitt <schmittjoaopedro@gmail.com>
2024-07-30 21:16:27 -0700

    code

---
+++ ml-pytorch-book/ch3-scikit-learn/logistic_regression_gd.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+class LogisticRegressionGD:
+    """
+    Gradient descent-based logistic regression classifier.
+    Notice this model is very similar to Adaline with the
+    difference that we use Sigmoid as activation and negative
+    log-likelihood as cost function.
+    """
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            # Gradient descent, if we calculate the derivatives for the log-likelihood
+            # we get the same formula as Adaline.
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            # Log-likelihood loss
+            loss = (-y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output))) / X.shape[0])
+            self.losses_.append(loss)
+
+        return self
+
+    def net_input(self, X):
+        # Also mentioned as Z in the notebook
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, z):
+        """Compute logistic sigmoid activation"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
